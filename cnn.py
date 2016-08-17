f76d251f3cd61d07cbaefdadbd127f0dde30016c
Roman Schulte-Sasse <sasse@molgen.mpg.de>
2016-08-17 17:36:20 +0200

    Added functionality for evaluating the model on unknown images.
    Furthermore, a ROC curve is plotted in an one-vs-all strategy.

--- cnn.py
+++ cnn.py
@@ -24,6 +24,7 @@ import time
 from datetime import datetime
 import matplotlib.pyplot as plt
 import os
+import numpy as np
 
 class cNN:
     """
@@ -44,54 +45,47 @@ class cNN:
             of neurons. For the convolution, the number
             of neurons corresponds to the number of kernels.
     """
-    def __init__(self, _architecture, _img_shape=(28, 28), _kernel_shape=(5, 5), _learning_rate=0.001):
-        if len(architecture) > 5:
-            print "ERROR. The network is too deep. So far, we can't deal with more than 5 layers!"
-        self.learning_rate = _learning_rate
-        self.architecture = _architecture
-        self.kernel_shape = _kernel_shape
-        
+    def __init__(self, path_to_model=None):
+
+	if not path_to_model is None:
+	    self.load_model
+
         # some variables which are set by the training function
-        self.img_shape = _img_shape  # (x, y, channels)
+        self.img_shape = (32, 32, 1)
+	self.kernel_shape = (5, 5)
         self.n_classes = 43
+	
+	# gradient descent parameters
+	self.learning_rate = 0.00001
+	self.batch_size = 20
+	self.num_epochs = 10000
+	self.dropout_prob = .5
+	
+	# some network properties
+        self.n_kernels_c1 = 108
+        self.n_kernels_c2 = 50
+        self.n_neurons_d1 = 64
+        self.pool_factor_1 = 4
+        self.pool_factor_2 = 2
 
-    """
-    This function generates lists of weight matrices and bias matrices from
-    some given architecture.
-    These can then be used to construct the network.
-    """
-    def generate_weights_and_biases(self):
-        weights = []
-        biases = []
-        for layer in xrange(len(self.architecture)):
-            if self.architecture[layer][0] == "conv":
-                if layer == 0:  # first layer
-                    last_output = self.img_shape[2]
-                else:
-                    last_output = self.architecture[layer-1][1]
-                weights.append(tf.Variable(tf.random_normal([self.kernel_shape[0],
-                                                            self.kernel_shape[1],
-                                                            last_output,
-                                                            self.architecture[layer][1]]
-                                                           )))
-                biases.append(tf.Variable(tf.random_normal([self.architecture[layer][1]])))
 
-            elif self.architecture[layer][0] == "dense":
-                last_output = self.architecture[layer-1][1]
-                num_of_conv_layers_so_far = len([i for i in xrange(layer) if self.architecture[i] == "conv"])
-                num_of_input_units = (img_shape[0] * img_shape[1]) / num_of_conv_layers_so_far**2
-                weights.append(tf.Variable(tf.random_normal([num_of_input_units, self.architecture[layer][1]])))
-                biases.append(tf.Variable(tf.random_normal([self.architecture[layer][1]])))
-            
-            elif self.architecture[layer][0] == "out":
-                assert layer == (len(architecture) - 1) # sanity check
-                last_output = self.architecture[layer-1][1]
-                weights.append(tf.Variable(tf.random_normal([last_output, self.architecture[layer][1]])))
-                biases.append(tf.Variable(tf.random_normal([self.architecture[layer][1]])))
-            else:
-                print "Unknown layer type! " + self.architecture[layer][0]
-        return weights, biases
-                
+    def sanity_check(self, image_shape, label_shape):
+	passed = True
+	if self.img_shape != image_shape:
+	    print "ERROR! Image shape does not correspond to predefined!"
+	    passed = False
+	
+	if self.n_classes != label_shape[1]:
+	    print "ERROR! Number of classes does not correspond to label shape!"
+	    passed = False
+	
+	if image_shape[0] != label_shape[0]:
+	    print "ERROR! Different number of training examples and labels!"
+	    passed = False
+
+	return passed
+
+
     def weight_variable(self, shape, name):
         initial = tf.truncated_normal(shape, stddev=0.1)
         return tf.Variable(initial, name=name)
@@ -106,21 +100,65 @@ class cNN:
     def max_pool_2x2(self, x, k=2):
         return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1], padding='SAME')
 
-    def construct_graph(self, images, labels, weights, biases, keep_prob):
-        last_output = images
-        for layer in xrange(len(self.architecture)):
-            local_weight = weights[layer]
-            local_bias = biases[layer]
-            if self.architecture[layer][0] == "conv":
-                convoluted = tf.nn.relu(self.conv2d(last_output, local_weight) + local_bias)
-                pooled = self.max_pool_2x2(convoluted, k=4) # FIXME: k arbitrary
-                current_output = pooled # for next layer
-            elif self.architecture[layer][0] == "dense":
-                dense_out = tf.nn.relu(tf.matmul(current_output, local_weight) + local_bias)
-            elif self.architecture[layer][0] == "out":
-                pass
-            else:
-                print "Unknown layer type! " + self.architecture[layer][0]
+    def load_model_and_evaluate(self, path, data):
+	x = tf.placeholder(tf.float32, shape=(self.batch_size, self.img_shape[0], self.img_shape[1], self.img_shape[2]), name="images")
+	keep_prob = tf.placeholder(tf.float32, name="dropout_prob")  # dropout (keep probability)
+
+	prediction_op = self.feed_forward(x, keep_prob)
+	batch_runs = data.shape[0] / self.batch_size
+	saver = tf.train.Saver()
+	with tf.Session() as sess:
+	    saver.restore(sess, path)
+	    print "Model successfully loaded!"
+	    
+	    # evaluate the data on our pretrained model
+	    predictions = np.zeros((data.shape[0], self.n_classes))
+	    for batchIdx in range(batch_runs):
+		local_predictions = sess.run(prediction_op,
+						feed_dict={x: data[
+							      batchIdx * self.batch_size:(batchIdx + 1) * self.batch_size],
+							   keep_prob: 1.})
+		predictions[batchIdx*self.batch_size:(batchIdx+1)*self.batch_size] = local_predictions
+	
+	    return predictions
+    
+    def feed_forward(self, images, keep_prob):
+	# create variables for layer one
+        W_conv1 = self.weight_variable([self.kernel_shape[0], self.kernel_shape[1], self.img_shape[2], self.n_kernels_c1], "kernels_layer1")
+        b_conv1 = self.bias_variable([self.n_kernels_c1], "biases_layer1")
+        
+        # do convolution
+        # and max pooling for layer one
+        h_conv1 = tf.nn.relu(self.conv2d(images, W_conv1) + b_conv1)
+        h_pool1 = self.max_pool_2x2(h_conv1, k=self.pool_factor_1)
+
+        # initialize vars for layer two
+        W_conv2 = self.weight_variable([self.kernel_shape[0], self.kernel_shape[1], self.n_kernels_c1, self.n_kernels_c2], "kernels_layer2")
+        b_conv2 = self.bias_variable([self.n_kernels_c2], "biases_layer2")
+
+        # convolve and max pool layer 2
+        h_conv2 = tf.nn.relu(self.conv2d(h_pool1, W_conv2) + b_conv2)
+        h_pool2 = self.max_pool_2x2(h_conv2, k=self.pool_factor_2)
+        
+        # now, do the dense layer 3
+        reduced_img_w = self.img_shape[0] / (self.pool_factor_1*self.pool_factor_2)
+        reduced_img_h = self.img_shape[1] / (self.pool_factor_1*self.pool_factor_2)
+        
+        W_fc1 = self.weight_variable([reduced_img_w * reduced_img_h * self.n_kernels_c2, self.n_neurons_d1], "weights_dense_layer3")
+        b_fc1 = self.bias_variable([self.n_neurons_d1], "biases_dense_layer3")
+        h_pool2_flat = tf.reshape(h_pool2, [-1, reduced_img_w*reduced_img_h*self.n_kernels_c2])
+        h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+        # apply dropout
+        h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+        
+        # out_layer
+        W_fc2 = self.weight_variable([self.n_neurons_d1, self.n_classes], "weights_out_layer4")
+        b_fc2 = self.bias_variable([self.n_classes], "biases_out_layer4")
+        y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+	
+	return y_conv
+
     """
     This function creates the graph for training and returns a tensorflow function object.
     This object can then be used to train the batches.
@@ -136,46 +174,7 @@ class cNN:
     """
     def construct_model(self, images, labels, keep_prob):
         
-        # some network properties
-        n_kernels_c1 = 108
-        n_kernels_c2 = 50
-        n_neurons_d1 = 64
-        pool_factor_1 = 4
-        pool_factor_2 = 2
-
-        # create variables for layer one
-        W_conv1 = self.weight_variable([kernel_shape[0], kernel_shape[1], self.img_shape[2], n_kernels_c1], "kernels_layer1")
-        b_conv1 = self.bias_variable([n_kernels_c1], "biases_layer1")
-        
-        # do convolution
-        # and max pooling for layer one
-        h_conv1 = tf.nn.relu(self.conv2d(images, W_conv1) + b_conv1)
-        h_pool1 = self.max_pool_2x2(h_conv1, k=pool_factor_1)
-
-        # initialize vars for layer two
-        W_conv2 = self.weight_variable([kernel_shape[0], kernel_shape[1], n_kernels_c1, n_kernels_c2], "kernels_layer2")
-        b_conv2 = self.bias_variable([n_kernels_c2], "biases_layer2")
-
-        # convolve and max pool layer 2
-        h_conv2 = tf.nn.relu(self.conv2d(h_pool1, W_conv2) + b_conv2)
-        h_pool2 = self.max_pool_2x2(h_conv2, k=pool_factor_2)
-        
-        # now, do the dense layer 3
-        reduced_img_w = self.img_shape[0] / (pool_factor_1*pool_factor_2)
-        reduced_img_h = self.img_shape[1] / (pool_factor_1*pool_factor_2)
-        
-        W_fc1 = self.weight_variable([reduced_img_w * reduced_img_h * n_kernels_c2, n_neurons_d1], "weights_dense_layer3")
-        b_fc1 = self.bias_variable([n_neurons_d1], "biases_dense_layer3")
-        h_pool2_flat = tf.reshape(h_pool2, [-1, reduced_img_w*reduced_img_h*n_kernels_c2])
-        h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-        # apply dropout
-        h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-        
-        # out_layer
-        W_fc2 = self.weight_variable([n_neurons_d1, self.n_classes], "weights_out_layer4")
-        b_fc2 = self.bias_variable([self.n_classes], "biases_out_layer4")
-        y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+	y_conv = self.feed_forward(images, keep_prob)
 
         # add cross entropy as objective function
         loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, labels))
@@ -183,31 +182,24 @@ class cNN:
         correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(labels, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-        return train_step, loss, accuracy
+        return train_step, loss, y_conv, accuracy
 
-    def train_model(self, train_images, train_label, test_images, test_label):
-
-	print train_images.shape
-	assert(train_images.shape[0] == train_label.shape[0])
+    def train_model(self, train_images, train_label, test_images, test_label, kernel_shape=(5, 5), learning_rate=0.001):
 	
 	# set some class variables before constructing the model
-	self.n_classes = train_label.shape[1]
-	self.img_shape = train_images[0].shape
+	if not self.sanity_check(train_images.shape, train_label.shape):
+	    return -1
+
 	train_size = train_images.shape[0]
-
-	batch_size = 20
-	num_epochs = 10000
-	dropout_prob = .5
-
-	batch_runs = train_size / batch_size
-	print "Batch size: " + str(batch_size)
+	batch_runs = train_size / self.batch_size
+	print "Batch size: " + str(self.batch_size)
 	print "Number of iterations per epoch: " + str(batch_runs)
 
 	# create the graph
-	x = tf.placeholder(tf.float32, shape=(batch_size, self.img_shape[0], self.img_shape[1], self.img_shape[2]), name="images")
-	y = tf.placeholder(tf.float32, shape=(batch_size, self.n_classes), name="labels")
+	x = tf.placeholder(tf.float32, shape=(self.batch_size, self.img_shape[0], self.img_shape[1], self.img_shape[2]), name="images")
+	y = tf.placeholder(tf.float32, shape=(self.batch_size, self.n_classes), name="labels")
 	keep_prob = tf.placeholder(tf.float32, name="dropout_prob")  # dropout (keep probability)
-	train_op, ce_op, accuracy_op = self.construct_model(x, y, keep_prob)
+	train_op, ce_op, accuracy_op, prediction_op = self.construct_model(x, y, keep_prob)
 	# Add ops to save and restore all the variables.
 	saver = tf.train.Saver()
 	print "Graph successfully constructed! Start training..."
@@ -218,40 +210,40 @@ class cNN:
 	with tf.Session() as sess:
 	    sess.run(tf.initialize_all_variables())
 	    with tf.device('/gpu:1'):
-		for epoch in range(num_epochs):
+		for epoch in range(self.num_epochs):
 		    for batchIdx in range(batch_runs):
-			sess.run(train_op, feed_dict={x: train_images[batchIdx*batch_size:(batchIdx+1)*batch_size],
-						      y: train_label[batchIdx*batch_size:(batchIdx+1)*batch_size],
-						      keep_prob: dropout_prob})
+			sess.run(train_op, feed_dict={x: train_images[batchIdx*self.batch_size:(batchIdx+1)*self.batch_size],
+						      y: train_label[batchIdx*self.batch_size:(batchIdx+1)*self.batch_size],
+						      keep_prob: self.dropout_prob})
 			
 			if batchIdx % (batch_runs / 2) == 0:  # the denominator determines the printing frequency (here twice every epoch)
 			    total_acc_test = 0.
 			    total_ce_test = 0.
 			    total_acc_train = 0.
-			    for test_batch in xrange(test_images.shape[0] / batch_size):
+			    for test_batch in xrange(test_images.shape[0] / self.batch_size):
 				total_acc_test += sess.run(accuracy_op,
 							   feed_dict={x: test_images[
-									 test_batch * batch_size:(test_batch + 1) * batch_size],
+									 test_batch * self.batch_size:(test_batch + 1) * self.batch_size],
 								      y: test_label[
-									 test_batch * batch_size:(test_batch + 1) * batch_size],
+									 test_batch * self.batch_size:(test_batch + 1) * self.batch_size],
 								      keep_prob: 1.})
 				total_ce_test += sess.run(ce_op,
 							  feed_dict={x: test_images[
-									test_batch * batch_size:(test_batch + 1) * batch_size],
+									test_batch * self.batch_size:(test_batch + 1) * self.batch_size],
 								     y: test_label[
-									test_batch * batch_size:(test_batch + 1) * batch_size],
+									test_batch * self.batch_size:(test_batch + 1) * self.batch_size],
 								     keep_prob: 1.})
-			    for train_batch in xrange(train_images.shape[0] / batch_size):
+			    for train_batch in xrange(train_images.shape[0] / self.batch_size):
 				total_acc_train += sess.run(accuracy_op,
 							    feed_dict={x: train_images[
-									train_batch * batch_size:(train_batch + 1) * batch_size],
+									train_batch * self.batch_size:(train_batch + 1) * self.batch_size],
 								       y: train_label[
-									train_batch * batch_size:(train_batch + 1) * batch_size],
+									train_batch * self.batch_size:(train_batch + 1) * self.batch_size],
 								       keep_prob: 1.})
 
-			    acc = total_acc_test / float(test_images.shape[0] / batch_size)
-			    ce = total_ce_test / float(test_images.shape[0] / batch_size)
-			    train_acc = total_acc_train / float(train_images.shape[0] / batch_size)
+			    acc = total_acc_test / float(test_images.shape[0] / self.batch_size)
+			    ce = total_ce_test / float(test_images.shape[0] / self.batch_size)
+			    train_acc = total_acc_train / float(train_images.shape[0] / self.batch_size)
 			    print "[Batch " + str(batchIdx) + "]\tAccuracy[Test]: " + str(acc) + "\tCross Entropy[Test]: " + str(ce) +\
 				  "\tAccuracy[Train]: " + str(train_acc)
 			    accuracies_test.append(acc)
@@ -262,7 +254,8 @@ class cNN:
 	    # save everything to directory
 	    self.finish_after_training(saver, sess, accuracies_test, accuracies_train, cross_entropy_test)
 
-
+    def evaluate_model(self, test_data):
+	pass
 
     def finish_after_training(self, saver, sess, accuracies_test, accuracies_train, cross_entropy_test):
 	
@@ -287,19 +280,14 @@ class cNN:
 
 
 
+if __name__ == "__main__":
+    print "Loading the data..."
+    with open('train_data_gray_norm_aug.pkl', 'rb') as train_handle:
+	train_set, train_labels = pickle.load(train_handle)
+    with open('test_data_gray_norm_aug.pkl', 'rb') as test_handle:
+	test_set, test_labels = pickle.load(test_handle)
+    print "Successfully loaded " + str(train_set.shape[0]) + " images!"
 
-print "Loading the data..."
-with open('train_data_gray_norm_aug.pkl', 'rb') as train_handle:
-    train_set, train_labels = pickle.load(train_handle)
-with open('test_data_gray_norm_aug.pkl', 'rb') as test_handle:
-    test_set, test_labels = pickle.load(test_handle)
-print "Successfully loaded " + str(train_set.shape[0]) + " images!"
-
-architecture = [("conv", 32), ("conv", 64), ("dense", 1024), ("out", 43)]
-img_shape = train_set[0].shape
-print "Images have shape: " + str(img_shape)
-kernel_shape = (5, 5)
-learning_rate = 0.00001
-classifier = cNN(architecture, img_shape, kernel_shape, learning_rate)
-classifier.train_model(train_set, train_labels, test_set, test_labels)
-print "Done with training!"
+    classifier = cNN()
+    classifier.train_model(train_set, train_labels, test_set, test_labels, kernel_shape, learning_rate)
+    print "Done with training!"
